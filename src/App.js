9d21eab5d5b47fd70cf1183532d2f3dbce13ea01
aylinyildiz <aylinyildiz1628@icloud.com>
2022-12-26 21:43:32 +0300

    product list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import{useState} from "react"
+import { Container, SimpleGrid, List, ThemeIcon } from "@mantine/core";
+import { IconCircleCheck, IconCircleDashed } from '@tabler/icons';
+import Card from "./components/Card";
+import "./App.css";
+
+const storeItems = [
+  {
+    name: "Ball",
+    price: 150,
+  },
+  {
+    name: "Chocolate",
+    price: 200,
+  },
+  {
+    name: "Shampoo",
+    price: 250,
+  },
+];
+
 
 function App() {
+  let [basketItems, setBasketItems] = useState([]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <SimpleGrid cols={3} className="Store">
+        {
+          storeItems.map(({name})=>{
+            return <Card key={name} name={name} onAdd={()=> setBasketItems([...basketItems,{name}])}/>
+          })
+        }
+      </SimpleGrid>
+      <List
+      className="list"
+      spacing="xs"
+      size="sm"
+      center
+      icon={
+        <ThemeIcon color="teal" size={24} radius="xl">
+          <IconCircleCheck size={16} />
+        </ThemeIcon>
+      }
+    >
+      {basketItems.map(({name}, index)=>(
+        <List.Item key={index}>{name}</List.Item>
+      ))}
+      
+     
+    </List>
+    </Container>
   );
 }
 
