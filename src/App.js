1bb603b98f0d1814bd127c0db0da03f1c1c57b9d
aylinyildiz <aylinyildiz1628@icloud.com>
2023-01-13 15:33:23 +0300

    The products in the basket were grouped.

--- src/App.js
+++ src/App.js
@@ -1,36 +1,53 @@
 import { useState } from "react";
-import {ActionIcon, Container, SimpleGrid, List, ThemeIcon, Input, Drawer, Group, Button,Indicator } from "@mantine/core";
+import {
+  Container,
+  SimpleGrid,
+  List,
+  ThemeIcon,
+  Input,
+  Drawer,
+  Group,
+  Button,
+  Indicator,
+  Badge,
+} from "@mantine/core";
 import { IconCircleCheck, IconShoppingCart } from "@tabler/icons";
 import Card from "./components/Card";
 import "./App.css";
 
 const storeItems = [
   {
+    id: 100,
     name: "Bottle",
     src: "bottle",
     price: 150,
   },
   {
+    id: 101,
     name: "Camera",
     src: "camera",
     price: 200,
   },
   {
+    id: 102,
     name: "Flower",
     src: "flower",
     price: 250,
   },
   {
+    id: 103,
     name: "Headphone",
     src: "headphone",
     price: 250,
   },
   {
+    id: 104,
     name: "Glasses",
     src: "glasses",
     price: 250,
   },
   {
+    id: 105,
     name: "Watch",
     src: "watch",
     price: 250,
@@ -41,27 +58,41 @@ function App() {
   let [opened, setOpened] = useState(false);
   let [basketItems, setBasketItems] = useState([]);
   let [searchValue, setSearchValue] = useState("");
-  let filteredItems = storeItems.filter((item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0);
+  let filteredItems = storeItems.filter(
+    (item) => item.name.toLowerCase().indexOf(searchValue.toLowerCase()) >= 0
+  );
+
+  let addToBasket = ({ id, name }) => {
+    let basketIndex = basketItems.findIndex((item) => item.id === id);
+    if (basketIndex >= 0) {
+      let _basketItems = [...basketItems];
+      _basketItems[basketIndex].count += 1;
+      setBasketItems(_basketItems);
+    } else {
+      setBasketItems([...basketItems, { id, name, count: 1 }]);
+    }
+  };
   return (
     <Container>
       <Group align="end">
-      <Input.Wrapper label="Search">
-        <Input onChange={(e)=>setSearchValue(e.target.value)} />
-      </Input.Wrapper>
-      <Button onClick={()=>setSearchValue("")}>Clean</Button>
-      <Indicator color="red" label={basketItems.length} size={22}>
-      <Button onClick={()=>setOpened(true)}><IconShoppingCart size={20} /> </Button>
-
-      </Indicator>
+        <Input.Wrapper label="Search">
+          <Input onChange={(e) => setSearchValue(e.target.value)} />
+        </Input.Wrapper>
+        <Button onClick={() => setSearchValue("")}>Clean</Button>
+        <Indicator color="red" label={basketItems.length} size={22}>
+          <Button onClick={() => setOpened(true)}>
+            <IconShoppingCart size={20} />{" "}
+          </Button>
+        </Indicator>
       </Group>
       <SimpleGrid cols={3} className="Store">
-        {filteredItems.map(({ name, src }) => {
+        {filteredItems.map(({ id, name, src }) => {
           return (
             <Card
               key={name}
               name={name}
               src={src}
-              onAdd={() => setBasketItems([...basketItems, { name }])}
+              onAdd={() => addToBasket({ id, name })}
             />
           );
         })}
@@ -75,23 +106,23 @@ function App() {
         size="xl"
       >
         <List
-        className="list"
-        spacing="xs"
-        size="sm"
-        center
-        icon={
-          <ThemeIcon color="teal" size={24} radius="xl">
-            <IconCircleCheck size={16} />
-          </ThemeIcon>
-        }
-      >
-        {basketItems.map(({ name }, index) => (
-          <List.Item key={index}>{name}</List.Item>
-        ))}
-      </List>
+          className="list"
+          spacing="xs"
+          size="sm"
+          center
+          icon={
+            <ThemeIcon color="teal" size={24} radius="xl">
+              <IconCircleCheck size={16} />
+            </ThemeIcon>
+          }
+        >
+          {basketItems.map(({ name, count }, index) => (
+            <List.Item key={index}>
+              {name} <Badge> {count}</Badge>
+            </List.Item>
+          ))}
+        </List>
       </Drawer>
-     
-     
     </Container>
   );
 }
